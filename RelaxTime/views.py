38e698dd95682953ee88ab977c5e77b5506485d1
Y-Rookie <2531145412@qq.com>
2016-04-15 14:27:08 +0800

    update static

--- RelaxTime/views.py
+++ RelaxTime/views.py
@@ -5,9 +5,11 @@ from service import niceview_svr
 class PictureView(TemplateView):
 
     template_name = 'index.html'
+    # template_name = 'test.html'
 
     def get_context_data(self, **kwargs):
         context = super(TemplateView, self).get_context_data(**kwargs)
         res = niceview_svr()
         context['pic_name'] = res['picList']
         return context
+
